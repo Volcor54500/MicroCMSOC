9c9117cf35f7a70a29e7aa2cc37e2855b10bdbb5
Volcor54500 <thierry@tam54.net>
2016-12-21 21:51:07 +0100

    intégration du framework Web Bootstrap

--- views/view.php
+++ views/view.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8" />
-    <link href="../web/microcms.css" rel="stylesheet" />
+    <link href="../web/lib/css/microcms.css" rel="stylesheet" />
     <title>MicroCMS - Home</title>
 </head>
 <body>
