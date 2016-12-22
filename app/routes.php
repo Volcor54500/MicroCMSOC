9742ada3046bb7934847ad7c47aa6ac25b665d61
Volcor54500 <thierry@tam54.net>
2016-12-22 12:50:27 +0100

    ajout de commentaires à un article

--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,8 @@
 <?php
+
 use Symfony\Component\HttpFoundation\Request;
+use MicroCMS\Domain\Comment;
+use MicroCMS\Form\Type\CommentType;
 
 // Home page
 $app->get('/', function () use ($app) {
@@ -8,11 +11,30 @@ $app->get('/', function () use ($app) {
 })->bind('home');
 
 // Article details with comments
-$app->get('/article/{id}', function ($id) use ($app) {
+$app->match('/article/{id}', function ($id, Request $request) use ($app) {
     $article = $app['dao.article']->find($id);
+    $commentFormView = null;
+    if ($app['security.authorization_checker']->isGranted('IS_AUTHENTICATED_FULLY')) {
+        // A user is fully authenticated : he can add comments
+        $comment = new Comment();
+        $comment->setArticle($article);
+        $user = $app['user'];
+        $comment->setAuthor($user);
+        $commentForm = $app['form.factory']->create(CommentType::class, $comment);
+        $commentForm->handleRequest($request);
+        if ($commentForm->isSubmitted() && $commentForm->isValid()) {
+            $app['dao.comment']->save($comment);
+            $app['session']->getFlashBag()->add('success', 'Your comment was successfully added.');
+        }
+        $commentFormView = $commentForm->createView();
+    }
     $comments = $app['dao.comment']->findAllByArticle($id);
-    return $app['twig']->render('article.html.twig', array('article' => $article, 'comments' => $comments));
-})->bind('article');;
+
+    return $app['twig']->render('article.html.twig', array(
+        'article' => $article,
+        'comments' => $comments,
+        'commentForm' => $commentFormView));
+})->bind('article');
 
 // Login form
 $app->get('/login', function(Request $request) use ($app) {
