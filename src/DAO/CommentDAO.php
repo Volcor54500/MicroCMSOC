b86f3e09e5ca26aaa75f35715ab73bb91b6d910d
Volcor54500 <thierry@tam54.net>
2016-12-22 19:10:17 +0100

    back-office d'administration

--- src/DAO/CommentDAO.php
+++ src/DAO/CommentDAO.php
@@ -4,7 +4,7 @@ namespace MicroCMS\DAO;
 
 use MicroCMS\Domain\Comment;
 
-class CommentDAO extends DAO
+class CommentDAO extends DAO 
 {
     /**
      * @var \MicroCMS\DAO\ArticleDAO
@@ -22,6 +22,24 @@ class CommentDAO extends DAO
 
     public function setUserDAO(UserDAO $userDAO) {
         $this->userDAO = $userDAO;
+    }
+
+    /**
+     * Returns a list of all comments, sorted by date (most recent first).
+     *
+     * @return array A list of all comments.
+     */
+    public function findAll() {
+        $sql = "select * from t_comment order by com_id desc";
+        $result = $this->getDb()->fetchAll($sql);
+
+        // Convert query result to an array of domain objects
+        $entities = array();
+        foreach ($result as $row) {
+            $id = $row['com_id'];
+            $entities[$id] = $this->buildDomainObject($row);
+        }
+        return $entities;
     }
 
     /**
@@ -53,6 +71,75 @@ class CommentDAO extends DAO
     }
 
     /**
+     * Returns a comment matching the supplied id.
+     *
+     * @param integer $id The comment id
+     *
+     * @return \MicroCMS\Domain\Comment|throws an exception if no matching comment is found
+     */
+    public function find($id) {
+        $sql = "select * from t_comment where com_id=?";
+        $row = $this->getDb()->fetchAssoc($sql, array($id));
+
+        if ($row)
+            return $this->buildDomainObject($row);
+        else
+            throw new \Exception("No comment matching id " . $id);
+    }
+
+    /**
+     * Saves a comment into the database.
+     *
+     * @param \MicroCMS\Domain\Comment $comment The comment to save
+     */
+    public function save(Comment $comment) {
+        $commentData = array(
+            'art_id' => $comment->getArticle()->getId(),
+            'usr_id' => $comment->getAuthor()->getId(),
+            'com_content' => $comment->getContent()
+            );
+
+        if ($comment->getId()) {
+            // The comment has already been saved : update it
+            $this->getDb()->update('t_comment', $commentData, array('com_id' => $comment->getId()));
+        } else {
+            // The comment has never been saved : insert it
+            $this->getDb()->insert('t_comment', $commentData);
+            // Get the id of the newly created comment and set it on the entity.
+            $id = $this->getDb()->lastInsertId();
+            $comment->setId($id);
+        }
+    }
+
+    /**
+     * Removes all comments for an article
+     *
+     * @param integer $articleId The id of the article
+     */
+    public function deleteAllByArticle($articleId) {
+        $this->getDb()->delete('t_comment', array('art_id' => $articleId));
+    }
+
+    /**
+     * Removes all comments for a user
+     *
+     * @param integer $userId The id of the user
+     */
+    public function deleteAllByUser($userId) {
+        $this->getDb()->delete('t_comment', array('usr_id' => $userId));
+    }
+
+    /**
+     * Removes a comment from the database.
+     *
+     * @param integer $id The comment id
+     */
+    public function delete($id) {
+        // Delete the comment
+        $this->getDb()->delete('t_comment', array('com_id' => $id));
+    }
+
+    /**
      * Creates an Comment object based on a DB row.
      *
      * @param array $row The DB row containing Comment data.
@@ -75,31 +162,7 @@ class CommentDAO extends DAO
             $user = $this->userDAO->find($userId);
             $comment->setAuthor($user);
         }
-
+        
         return $comment;
-    }
-
-    /**
-     * Saves a comment into the database.
-     *
-     * @param \MicroCMS\Domain\Comment $comment The comment to save
-     */
-    public function save(Comment $comment) {
-        $commentData = array(
-            'art_id' => $comment->getArticle()->getId(),
-            'usr_id' => $comment->getAuthor()->getId(),
-            'com_content' => $comment->getContent()
-        );
-
-        if ($comment->getId()) {
-            // The comment has already been saved : update it
-            $this->getDb()->update('t_comment', $commentData, array('com_id' => $comment->getId()));
-        } else {
-            // The comment has never been saved : insert it
-            $this->getDb()->insert('t_comment', $commentData);
-            // Get the id of the newly created comment and set it on the entity.
-            $id = $this->getDb()->lastInsertId();
-            $comment->setId($id);
-        }
     }
 }
